ab1b6e0cf51fb37370c4bc41476c30d172bf1325
Lukasz Kurek <lukascoorek@gmail.com>
2019-08-27 17:41:52 +0200

    test routes

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 
 //Connect DB
 connectDB();
+app.get('/', (req, res) => res.send('test'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => res.send('API Runing'));
-
 app.listen(PORT, () => console.log(`Server is running at port: ${PORT}`));
