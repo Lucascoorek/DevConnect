2f7a6e5e985830894bd66190389cfe3e9d38cf8b
Lukasz Kurek <lukascoorek@gmail.com>
2019-08-27 18:30:30 +0200

    user register route validtor

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route    GET api/users
-// @desc     Test route
+// @route    POST api/users
+// @desc     Register User
 // @acess    Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Email not valid').isEmail(),
+    check('password', 'Password must be min 6 chars').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
