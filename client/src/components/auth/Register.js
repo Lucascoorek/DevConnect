a4cfe4a310e9e1c27360f5e80a31be6f1669ac03
Lukasz Kurek <lukascoorek@gmail.com>
2019-09-16 15:46:47 +0200

    Login form fix on register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,29 +11,13 @@ const Register = () => {
   const { name, email, password, password2 } = formData;
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
-  const onSubmit = async e => {
+  const onSubmit = e => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Pass ain't match");
       return;
     }
-    const newUser = {
-      name,
-      email,
-      password
-    };
-    try {
-      const config = {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-      const body = JSON.stringify(newUser);
-      const res = await axios.post('/api/users', body, config);
-      console.log(res.data);
-    } catch (error) {
-      console.error(error.response.data);
-    }
+    console.log('Success');
   };
   return (
     <>
@@ -89,7 +73,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </>
   );
