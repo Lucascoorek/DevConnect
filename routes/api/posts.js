a112c8470580370684b715c69436bb0d5df929ed
Lukasz Kurek <lukascoorek@gmail.com>
2019-10-01 10:21:47 +0200

    Post Reducer, Action & Initial Component

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,7 +44,7 @@ router.post(
 // @route    GET api/posts
 // @desc     Get all posts
 // @acess    Private
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
     const posts = await Post.find().sort({ date: -1 });
     return res.json(posts);
